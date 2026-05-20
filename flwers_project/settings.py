b42fc314074814f5a1b90419bbd98b24835db51a
devbmv <maik_775@yahoo.com>
2026-05-20 14:42:18 +0100

    security: remove secrets from tree, untrack db/staticfiles, env-based config

--- flwers_project/settings.py
+++ flwers_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-dev-only-change-in-env")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
